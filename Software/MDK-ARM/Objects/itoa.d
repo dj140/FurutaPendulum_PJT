9c3251da6df2a568723e106b42d8dd65133b73cf
Dj140 <1613270283@qq.com>
2022-02-21 23:57:43 +0800

    update .gitignore

--- Software/MDK-ARM/Objects/itoa.d
+++
@@ -1,3 +0,0 @@
-.\objects\itoa.o: ..\ArduinoAPI\itoa.c
-.\objects\itoa.o: ..\ArduinoAPI\itoa.h
-.\objects\itoa.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
--- Software/MDK-ARM/Objects/wstring.d
+++
@@ -1,9 +0,0 @@
-.\objects\wstring.o: ..\ArduinoAPI\WString.cpp
-.\objects\wstring.o: ..\ArduinoAPI\WString.h
-.\objects\wstring.o: ..\ArduinoAPI\dtostrf.h
-.\objects\wstring.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\wstring.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\objects\wstring.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\wstring.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
-.\objects\wstring.o: ..\ArduinoAPI\itoa.h
-.\objects\wstring.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
